d715cf3db7f671bd2b8c1f066575c0b3e173f377
Bruno <brunobentosilva459@gmail.com>
2026-06-04 22:23:51 -0300

    alteracao do codigo

--- leituraOtica.c
+++ leituraOtica.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-    int n,cont;
+    int n,letra,cont,verificador = 0;
     int vet[5];
 
     scanf("%d",&n);
@@ -10,39 +10,40 @@ int main(){
     while(n != 0){
 
         for(int i = 0; i < n; i++){
+            letra = -1;
             cont = 0;
             for(int j = 0; j < 5; j++){
                 scanf("%d",&vet[j]);
 
                 if(vet[j] <= 127){
+                    letra = j;
                     cont++;
+                }else{
+                    verificador++;
                 }
-
             }
 
-            if(cont == 0 || cont > 2){
+            if(cont == 0 || cont >= 2 || verificador == 0){
                 printf("*\n");
             }else{
-                switch(cont){
-                    case 1:
+                switch(letra){
+                    case 0:
                         printf("A\n"); 
                         break;
-                    case 2:
+                    case 1:
                         printf("B\n");
                         break;
-                    case 3:
+                    case 2:
                         printf("C\n");
                         break;
-                    case 4:
+                    case 3:
                         printf("D\n");
                         break;
-                    case 5:
+                    case 4:
                         printf("E\n");
                         break;
                 }
             }
-
-
         }
 
         scanf("%d",&n);
